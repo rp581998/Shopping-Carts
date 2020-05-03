a2214cbb92055cd0c6ebc3dee00f5dc42908c6f0
Rajan Patel <rp581998@gmail.com>
2020-05-04 00:03:30 +0530

    added cart

--- script.js
+++ script.js
@@ -1,2 +1,49 @@
-console.log("HI");
-console.log('Go Corona')
+let carts = document.querySelectorAll(".add-cart");
+
+let products = [
+    {
+        name: 'Red Tshirt',
+        tag: 'redtshirt',
+        price: 15,
+        inCart: 0
+    },
+    {
+        name: 'Yellow Tshirt',
+        tag: 'yellowtshirt',
+        price: 20,
+        inCart: 0
+    },
+    {
+        name: 'Brown Tshirt',
+        tag: 'browntshirt',
+        price: 15,
+        inCart: 0
+    },
+    {
+        name: 'Red Shirt',
+        tag: 'redshirt',
+        price: 20,
+        inCart: 0
+    }
+];
+
+for (let i=0; i < carts.length; i++) {
+    carts[i].addEventListener('click', cartNumbers)
+}
+
+function cartNumbers() {
+    let productNumbers = localStorage.getItem('cartNumbers');
+    
+
+    productNumbers = parseInt(productNumbers);
+    
+    
+    if (productNumbers) {
+       localStorage.setItem('productNumbers', productNumbers + 1);
+        document.querySelector('.cart span').textContent = productNumbers + 1;
+    }
+    else {
+        localStorage.setItem('productNumbers', 1);
+        document.querySelector('.cart span').textContent = 1;
+    }
+}
